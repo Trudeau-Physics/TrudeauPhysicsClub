c21c9f0304fce0ed227f9e3ec4e41d8e16990187
348670050 <haloday873@gmail.com>
2023-04-18 23:55:52 -0400

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-      './routes/**/*.{html,js}',
+      './public/**/*.{html,js}',
       './src/components/**/*.js'
     ],
     presets: [],
