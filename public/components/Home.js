3adc98dff3d694021224e34d3bd96c8f598da66d
348670050 <haloday873@gmail.com>
2023-04-19 22:14:30 -0400

    STUFF

--- public/components/Home.js
+++ public/components/Home.js
@@ -1,4 +1,4 @@
 import { Hero } from './Hero.js';
 export function Home() {
-  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement(Hero, null));
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement(Hero, null), /*#__PURE__*/React.createElement(Hero, null));
 }
