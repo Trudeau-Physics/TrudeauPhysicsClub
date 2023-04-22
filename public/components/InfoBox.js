96e89e1e337da0fd28e8e4b1a651e41af0560205
unknown <flyingbikeddd@gmail.com>
2023-04-22 01:23:07 -0400

    info

--- public/components/InfoBox.js
+++ public/components/InfoBox.js
@@ -1,9 +1,11 @@
 export function InfoBox(props) {
   return /*#__PURE__*/React.createElement("div", {
-    className: "flex m-2 p-4 pt-6 h-1/2 w-1/2 border-2 border-green-50 rounded-2xl box-border flex-grow duration-700"
+    className: "flex m-2 p-4 pt-6 h-1/4 w-2/5 border-2 border-green-50 rounded-2xl box-border flex-grow duration-700"
   }, /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("div", {
+    className: "h-1/2 w-1/2"
+  }, /*#__PURE__*/React.createElement("div", {
     className: "text-6xl"
-  }, props.title), /*#__PURE__*/React.createElement("div", {
+  }, props.title)), /*#__PURE__*/React.createElement("div", {
     className: "text-2xl"
   }, props.children), /*#__PURE__*/React.createElement("image", {
     src: "something"
