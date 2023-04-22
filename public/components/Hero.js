1f7308971ee130a25d261da4361fd3ad291c3823
unknown <flyingbikeddd@gmail.com>
2023-04-22 12:54:20 -0400

    placeholderfont

--- public/components/Hero.js
+++ public/components/Hero.js
@@ -3,7 +3,7 @@ export function Hero() {
     id: "home",
     className: "pt-32 pb-32 snap-center z-[1] w-screen h-screen flex flex-col justify-around items-center "
   }, /*#__PURE__*/React.createElement("div", {
-    className: "font-extrabold text-transparent text-3xl sm:text-5xl lg:text-9xl bg-clip-text bg-gradient-to-r from-cyan-400 to-pink-600 duration-700"
+    className: "titlefont font-extrabold text-transparent text-3xl sm:text-5xl lg:text-9xl bg-clip-text bg-gradient-to-r from-cyan-400 to-pink-600 duration-700"
   }, "TRUDEAU PHYSICS"), /*#__PURE__*/React.createElement("div", {
     className: "font-bold text-1xl sm:text-1xl lg:text-3xl"
   }, "Subtitles"), /*#__PURE__*/React.createElement("div", {
