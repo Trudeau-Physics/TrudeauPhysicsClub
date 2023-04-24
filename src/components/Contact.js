5bb469941829fe18065863ec325d2b20cc299462
348670050 <haloday873@gmail.com>
2023-04-23 23:57:15 -0400

    bruh

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,7 @@
 
 export function Contact() {
     return (
-        <div id="contact" className='snap-center w-screen h-screen flex flex-col justify-around items-center bg-cyan-800' style={{ padding: '10vh' }}>
+        <div id="contact" className='snap-center w-screen h-screen min-h-[500px] flex flex-col items-center bg-cyan-800 overflow-visible' style={{ padding: '10vh' }}>
             <div className="font-extrabold text-center text-3xl sm:text-5xl lg:text-8xl duration-700">Contact</div>
             <div className="grid grid-cols-1 grid-rows-4 lg:grid-rows-1 lg:grid-cols-4 justify-center items-center w-full h-4/5 lg:h-3/5 p-4 gap-2 lg-p-8 lg:gap-8 ">
 
@@ -27,7 +27,7 @@ export function Contact() {
 function InfoBox(props) {
 
     return (
-        <div className='shadow-2xl transition ease-in-out p-4 flex lg:flex-col justify-between h-full w-full border-2 border-green-50 rounded-2xl box-border transform hover:-translate-y-1 hover:scale-105 hover:bg-opacity-80 hover:shadow-2xl hover:text-slate-900 hover:bg-slate-400 duration-200'>
+        <div className='shadow-2xl transition ease-in-out p-4 flex lg:flex-col justify-between min-h-auto min-w-auto h-full w-full border-2 border-green-50 rounded-2xl box-border transform hover:-translate-y-1 hover:scale-105 hover:bg-opacity-80 hover:shadow-2xl hover:text-slate-900 hover:bg-slate-400 duration-200'>
             <div className="flex flex-col lg:items-center lg:justify-around flex-grow">
                 <div className="text-xl lg:text-3xl lg:text-center">{props.title}</div>
                 <div className="text-base lg:text-xl underline">{props.children}</div>
