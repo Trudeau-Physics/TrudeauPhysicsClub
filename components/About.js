444cd80bbba5c537e81b737950e8461c1675bdfc
348670050 <haloday873@gmail.com>
2023-04-23 22:33:00 -0400

    Updates

--- components/About.js
+++ components/About.js
@@ -6,10 +6,14 @@ export function About() {
       padding: '10vh'
     }
   }, /*#__PURE__*/React.createElement("div", {
-    className: "font-black text-3xl sm:text-5xl lg:text-8xl flex justify-center items-center w-full h-1/5 lg:h-1/3 duration-700"
+    className: "font-black text-3xl sm:text-5xl lg:text-8xl flex justify-center items-center w-full h-1/6 lg:h-1/3 duration-700"
   }, "ABOUT US"), /*#__PURE__*/React.createElement("div", {
-    className: "grid grid-cols-1 grid-rows-4 lg:grid-rows-1 lg:grid-cols-4 justify-center items-center w-6/7 h-4/5 lg:h-2/3 p-4 gap-2 lg-p-8 lg:gap-8 "
-  }, "About Us Info")));
+    className: "grid grid-cols-1 grid-rows-4 lg:grid-rows-1 lg:grid-cols-2 justify-center items-center w-full h-5/6 lg:h-2/3 p-4 gap-2 lg-p-8 lg:gap-8 "
+  }, /*#__PURE__*/React.createElement(InfoBox, {
+    title: "About Us"
+  }, "This club\u2019s ultimate goal is to create a welcoming and safe space in Trudeau, in this planet, where everyone can feel free to share and talk about their ideas and observations about the world. We believe that physics is not only about the mathematical calculations, but also about the nature and philosophy of this world. We are also here to lift each other up, and help each other through difficult times. No one is bad at physics!"), /*#__PURE__*/React.createElement(InfoBox, {
+    title: "What we do"
+  }, "1. Hire execs 2. Kick all members 3. Bully Martin"))));
 }
 function InfoBox(props) {
   return /*#__PURE__*/React.createElement("div", {
@@ -18,5 +22,13 @@ function InfoBox(props) {
     className: "flex flex-col lg:items-center lg:justify-around flex-grow"
   }, /*#__PURE__*/React.createElement("div", {
     className: "text-xl lg:text-3xl lg:text-center"
-  }, props.title)));
+  }, props.title), /*#__PURE__*/React.createElement("div", {
+    className: "text-base lg:text-xl"
+  }, props.children)), /*#__PURE__*/React.createElement("div", {
+    className: "flex justify-center flex-col lg:flex-row lg:w-full"
+  }, /*#__PURE__*/React.createElement("img", {
+    src: props.iconUrl,
+    alt: "Icon",
+    className: "w-16 h-16 hover:h-20 hover:w-20 lg:w-24 lg:h-24 lg:hover:h-36 lg:hover:w-36 duration-100"
+  })));
 }
