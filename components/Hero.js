916fb1811ae4406901e24fce89a367a3644e0ac1
348670050 <haloday873@gmail.com>
2023-05-30 17:41:12 -0400

    Updates

--- components/Hero.js
+++ components/Hero.js
@@ -3,8 +3,14 @@ export function Hero() {
     id: "home",
     className: "z-[1]w-screen h-screen min-h-[800px] flex flex-col justify-center items-center"
   }, /*#__PURE__*/React.createElement("div", {
+<<<<<<< HEAD
+    className: "titlefont font-extrabold text-transparent text-center text-3xl sm:text-5xl lg:text-8xl bg-clip-text bg-gradient-to-r from-cyan-400 to-pink-600 duration-700"
+  }, "TRUDEAU PHYSICS"), /*#__PURE__*/React.createElement("div", {
+    className: "text-cyan-400 font-bold text-1xl sm:text-1xl lg:text-3xl"
+=======
     className: "titlefont font-extrabold text-transparent text-center text-5xl sm:text-5xl lg:text-8xl bg-clip-text bg-gradient-to-r from-[#ffd89b] to-[#19547b] duration-700"
   }, "TRUDEAU PHYSICS"), /*#__PURE__*/React.createElement("div", {
     className: "text-[#F0F0F0] font-bold text-1xl sm:text-1xl lg:text-3xl"
+>>>>>>> d58c2e2104018043bef1697dfa75f6798e580777
   }, "Room 122 - Every Monday?"));
 }
