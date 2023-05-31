22f4571361271c93aa111d99b28022a2e671c7a2
348670050 <haloday873@gmail.com>
2023-05-30 22:34:02 -0400

    Updates

--- components/About.js
+++ components/About.js
@@ -11,11 +11,7 @@ export function About() {
     className: "grid grid-cols-1 grid-rows-2 lg:grid-rows-1 lg:grid-cols-2 w-full h-5/6 lg:h-2/3 p-16 lg:p-24 gap-8 lg:gap-8"
   }, /*#__PURE__*/React.createElement(InfoBox, {
     title: "About Us"
-<<<<<<< HEAD
   }, "We believe that physics is not only about the mathematical calculations, but also about the nature and philosophy of this world. This club\u2019s ultimate goal is to create a welcoming and safe space in Trudeau, in this planet, where everyone can feel free to share and talk about their ideas and observations about the world. We are also here to lift each other up, and help each other through difficult times. No one is bad at physics!"), /*#__PURE__*/React.createElement(InfoBox, {
-=======
-  }, "Trudeau Physics is a community of students, professionals, and enthusiasts passionate about the fascinating world of physics. Our goal is to provide a platform for learning, discussion, and experimentation in physics-related topics. Whether you're interested in astrophysics, quantum mechanics, or anything in between, we welcome you to join us in exploring the wonders of the universe. Through our events, workshops, and activities, we strive to foster a love for physics and inspire the next generation of physicists. Come discover the beauty and complexity of the physical world with us!"), /*#__PURE__*/React.createElement(InfoBox, {
->>>>>>> d58c2e2104018043bef1697dfa75f6798e580777
     title: "What we do"
   }, "1. Hire execs 2. Kick all members 3. Bully Martin"))));
 }
--- components/App.js
+++ components/App.js
@@ -2,10 +2,7 @@ import { Hero } from './Hero.js';
 import { Contact } from './Contact.js';
 import { About } from './About.js';
 import { NavBar } from './NavBar.js';
-<<<<<<< HEAD
 import { Team } from './Team.js';
-=======
->>>>>>> d58c2e2104018043bef1697dfa75f6798e580777
 function App() {
   return /*#__PURE__*/React.createElement("div", {
     className: "scroll-smooth snap-mandatory snap-y"
@@ -20,11 +17,7 @@ function App() {
     type: "video/mp4"
   })), /*#__PURE__*/React.createElement("div", {
     className: "caption"
-<<<<<<< HEAD
   }, /*#__PURE__*/React.createElement(NavBar, null), /*#__PURE__*/React.createElement(Hero, null), /*#__PURE__*/React.createElement(About, null), /*#__PURE__*/React.createElement(Team, null), /*#__PURE__*/React.createElement(Contact, null))));
-=======
-  }, /*#__PURE__*/React.createElement(NavBar, null), /*#__PURE__*/React.createElement(Hero, null), /*#__PURE__*/React.createElement(About, null), /*#__PURE__*/React.createElement(Contact, null))));
->>>>>>> d58c2e2104018043bef1697dfa75f6798e580777
 }
 const root = ReactDOM.createRoot(document.querySelector('#app'));
 console.log(root);
